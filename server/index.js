392ec048e2de51fc9b1de017a8f1fbadc95f18f6
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-22 19:56:35 -0700

    init server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const os = require('os');
+const compression = require('compression');
+const bunyan = require('bunyan');
+
+const app = express();
+const log = bunyan.createLogger({ name: 'production' });
+const port = process.env.PORT || 3000;
+
+app.use(compression());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  } else {
+    log.info(`Listening at http://${os.networkInterfaces().lo0[0].address}:${port}`);
+  }
+});
