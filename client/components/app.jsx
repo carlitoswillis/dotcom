87cfb5c54ed75deff9c67d7934791001818b0bb3
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-30 00:38:49 -0700

    style more

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,76 +7,123 @@ import About from './about';
 import Blog from './blog';
 import Portfolio from './portfolio';
 
-const App = () => (
-  <Router>
-    <div>
-      <div className="intro">
-        <Nav />
-      </div>
-      <Switch>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+  }
 
-        <Route path="/about" component={About} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/shop" component={Shop} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/">
-          <div className="home">
-            <div className="profile">
-              <img className="profpic" src="images/profile.jpg" alt="me" />
+  render() {
+    const { show } = this.state;
+    return (
+      <Router>
+        {show
+          ? (
+            <div onClick={() => this.setState({ show: false })} className="fullScreenNav">
+              <div className="menuSplit">
+                <h1 className="title">
+                  carlitos willis | software engineer
+                </h1>
+                <Nav classy="classy" />
+              </div>
             </div>
-            <div className="shortAbout">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type
-              and scrambled it to make a type specimen book.
-              It has survived not only five centuries,
-              but also the leap into electronic typesetting, remaining essentially unchanged.
-              It was popularised in the 1960s with the release of
-              Letraset sheets containing Lorem Ipsum passages,
-              and more recently
-              with desktop publishing software like
-              Aldus PageMaker including versions of Lorem Ipsum.
+          )
+          : <div />}
+        <div>
+          <div className="introToo">
+            <h1 className="titleTwo">
+              carlitos willis | software engineer
+            </h1>
+          </div>
+          <div className="intro">
+            <div
+              onClick={() => {
+                console.log('hey');
+                this.setState({
+                  show: !show,
+                });
+              }}
+              className="hamburger"
+            >
+              <div className="line" />
+              <div className="line" />
+              <div className="line" />
             </div>
-            <div className="fields">
-              <div className="field">
-                <img src="images/se.jpeg" alt="thing i do" />
-                <div className="pair">
-                  <h2>
-                    Thing I do
-                  </h2>
-                  <p>
-                    background info i suppose
-                  </p>
-                </div>
-              </div>
-              <div className="field">
-                <img src="images/smma.png" alt="thing i do" />
-                <div className="pair">
-                  <h2>
-                    Thing I do 2
-                  </h2>
-                  <p>
-                    background info i suppose 2
-                  </p>
-                </div>
-              </div>
-              <div className="field">
-                <img src="images/push.jpg" alt="thing i do" />
-                <div className="pair">
-                  <h2>
-                    Thing I do 3
-                  </h2>
-                  <p>
-                    background info i suppose 3
-                  </p>
-                </div>
-              </div>
+            <div className="menuSplit">
+              <h1 className="title">
+                carlitos willis | software engineer
+              </h1>
+              <Nav />
             </div>
           </div>
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-);
+          <Switch>
+
+            <Route path="/about" component={About} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/shop" component={Shop} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/">
+              <div className="home">
+                <div className="profile">
+                  <img className="profpic" src="images/profile.jpg" alt="me" />
+                </div>
+                <div className="shortAbout">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
+                  when an unknown printer took a galley of type
+                  and scrambled it to make a type specimen book.
+                  It has survived not only five centuries,
+                  but also the leap into electronic typesetting, remaining essentially unchanged.
+                  It was popularised in the 1960s with the release of
+                  Letraset sheets containing Lorem Ipsum passages,
+                  and more recently
+                  with desktop publishing software like
+                  Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
+                <div className="fields">
+                  <div className="field">
+                    <img src="images/se.jpeg" alt="thing i do" />
+                    <div className="pair">
+                      <h2>
+                        Thing I do
+                      </h2>
+                      <p>
+                        background info i suppose
+                      </p>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <img src="images/smma.png" alt="thing i do" />
+                    <div className="pair">
+                      <h2>
+                        Thing I do 2
+                      </h2>
+                      <p>
+                        background info i suppose 2
+                      </p>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <img src="images/push.jpg" alt="thing i do" />
+                    <div className="pair">
+                      <h2>
+                        Thing I do 3
+                      </h2>
+                      <p>
+                        background info i suppose 3
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
---
+++ client/components/hamburger.jsx
@@ -0,0 +1,11 @@
+import { React, useState } from 'react';
+
+const Hamburger = () => (
+  <div className="hamburger">
+    <div className="line" />
+    <div className="line" />
+    <div className="line" />
+  </div>
+);
+
+export default Hamburger;
