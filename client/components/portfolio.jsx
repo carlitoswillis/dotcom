6a65c0179fa011d87cf09c3bc75e3ddcf4f15566
carlitoswillis <carlitoswillis@berkeley.edu>
2020-07-05 21:08:50 -0700

    try carousels for portfolio

--- client/components/portfolio.jsx
+++ client/components/portfolio.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const Portfolio = () => (<div>Portfolio</div>);
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        {new Array(10).fill(0).map(x => (<div>empty</div>))}
+      </div>
+    );
+  }
+}
 
 export default Portfolio;
