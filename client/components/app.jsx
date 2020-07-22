bc7addead4dc5a96d67a0fe179db7dce346d05a9
carlitoswillis <carlitoswillis@berkeley.edu>
2020-07-21 20:04:41 -0700

    make about landing page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -72,33 +72,21 @@ class App extends React.Component {
             {/* <Route path="/resume" component={Resume} /> */}
             {/* <Route path="/blog" component={Blog} /> */}
             <Route path="/portfolio" component={Portfolio} />
-            <Route path="/">
+            <Route path="/" component={About} />
+            {/* <Route path="/">
               <div className="home">
                 <div className="profile">
                   <a href="/about">
                     <img className="profpic" src="images/profile.jpg" alt="me" />
                   </a>
                 </div>
-                {/* <div className="shortAbout">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                  Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
-                  when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book.
-                  It has survived not only five centuries,
-                  but also the leap into electronic typesetting, remaining essentially unchanged.
-                  It was popularised in the 1960s with the release of
-                  Letraset sheets containing Lorem Ipsum passages,
-                  and more recently
-                  with desktop publishing software like
-                  Aldus PageMaker including versions of Lorem Ipsum.
-                </div> */}
                 <div className="fields">
                   <Modal img="se.jpeg" field="Software Engineer" info="I am a great software engineer as you can see by this site" />
                   <Modal img="smma.jpeg" field="Digital Marketer" info="I run very dope ads" />
                   <Modal img="push.jpg" field="Music Producer" info="I make very nice musics" />
                 </div>
               </div>
-            </Route>
+            </Route> */}
           </Switch>
         </div>
       </Router>
